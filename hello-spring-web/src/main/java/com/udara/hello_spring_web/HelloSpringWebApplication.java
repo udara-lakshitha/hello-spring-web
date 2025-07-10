f759c0e59000e6c607b3eccd7b2055ce7139f0ae
udara-lakshitha <lakshithau95@gmail.com>
2025-07-10 15:37:23 +0530

    Feat: Create initial GreetingController with two GET endpoints

---
+++ hello-spring-web/src/main/java/com/udara/hello_spring_web/HelloSpringWebApplication.java
@@ -0,0 +1,13 @@
+package com.udara.hello_spring_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringWebApplication.class, args);
+	}
+
+}
---
+++ hello-spring-web/src/test/java/com/udara/hello_spring_web/HelloSpringWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udara.hello_spring_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
